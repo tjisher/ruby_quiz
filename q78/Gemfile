09135172046829abe123da5344179760a08c8f57
tjisher <tjisher@gmail.com>
2016-02-20 20:23:10 +0000

    q78 Basic Tests

---
+++ q78/Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+ruby '2.2.4'
+
+group :test do
+  gem "minitest"
+  gem "rake"
+end
--- q78/validate_box_tests.rb
+++ q78/validate_box_tests.rb
@@ -1,1 +1,114 @@
+##TOC
+#methods and empty payload
+#basic types
+#nested types
+#orignal specification
+
+#TOD#
+# all iterations
+# validate matches valid?
+# ? change must_output to check exit code
+
 require_relative 'validate_box.rb'
+require 'minitest/spec'
+require 'minitest/autorun'
+
+describe Box do
+	#methods and empty payload
+	it "answers to valid?" do
+		Box.must_respond_to(:valid?, "")
+	end
+	it "answers to validate" do
+		Box.must_respond_to(:validate, "")
+	end
+
+	it "fails empty strings" do
+		Box.valid?("").must_be_same_as(false)
+	end
+
+	#basic types
+	it "passes soft wraping" do
+		Box.valid?("(B)").must_be_same_as(true)
+	end
+
+	it "passes cardboard box" do
+		Box.valid?("[B]").must_be_same_as(true)
+	end
+
+	it "passes wooden box" do
+		Box.valid?("{B}").must_be_same_as(true)
+	end
+
+	it "fails unknown boxing" do
+		Box.valid?("|B|").must_be_same_as(false)
+	end
+
+	it "fails unknown boxed item" do
+		Box.valid?("{Z}").must_be_same_as(false)
+	end
+
+	it "fails when nothing wrapped" do
+		Box.valid?("{}").must_be_same_as(false)
+	end
+
+	it "fails when there is no wrapping" do
+		Box.valid?("B").must_be_same_as(false)
+	end
+
+	it "fails when warpping is started but not closed" do
+		Box.valid?("(B").must_be_same_as(false)
+		#todo# all iterations)
+	end
+
+	it "fails when warpping is not started" do
+		Box.valid?("B)").must_be_same_as(false)
+		#todo# all iterations
+	end
+
+	#nested types
+	it "passes item inside two wrappings" do
+		Box.valid?("{(B)}").must_be_same_as(true)
+		#todo# all iterations
+	end
+
+	it "passes item inside three wrappings" do
+		Box.valid?("[{(B)}]").must_be_same_as(true)
+	end
+
+	it "passes two wrapped items" do
+		Box.valid?("{(B)(B)}").must_be_same_as(true)
+		#todo# all iterations
+	end
+
+	it "passes multiple wrapped items" do
+		Box.valid?("[{(B)}{(B)(B)}]").must_be_same_as(true)
+		#todo# all iterations
+	end
+
+	it "fails when nested wrapping is started but not closed" do
+		Box.valid?("{(B)").must_be_same_as(false)
+		#todo# all iterations
+	end
+
+	it "fails when nested wrapping is closed but not started" do
+		Box.valid?("(B)}").must_be_same_as(false)
+		#todo# all iterations
+	end
+
+	#orignal specification
+	it "passes and returns 0 for orginal specification example 1: [{(B)}{(B)(B)}]" do
+		Box.valid?("[{(B)}{(B)(B)}]").must_be_same_as(true)
+		Box.validate("[{(B)}{(B)(B)}]").must_output(0)
+	end
+
+	it "fails and returns 1 for orignal specification example 2: [{(B}{(B)(B)}]" do
+		Box.valid?("[{(B}{(B)(B)}]").must_be_same_as(false)
+		Box.validate("[{(B}{(B)(B)}]").must_output(1)
+	end
+
+	it "fails and returns 1 for orignal specification example 3: {(B)}{(B)(B)}]" do
+		Box.valid?("{(B)}{(B)(B)}]").must_be_same_as(false)
+		Box.validate("{(B)}{(B)(B)}]").must_output(1)
+	end
+
+end
